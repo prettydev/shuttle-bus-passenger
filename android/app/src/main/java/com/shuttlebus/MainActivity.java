7f715330fa10d2b3b72a8d1eb878d55315a25627
MingChuan Jiang <trueshinygold@gmail.com>
2019-12-31 12:38:50 +0800

    add app icon

--- android/app/src/main/java/com/shuttlebus/MainActivity.java
+++ android/app/src/main/java/com/shuttlebus/MainActivity.java
@@ -1,9 +1,13 @@
 package com.creditcoder.shuttlebus;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
+
 
 public class MainActivity extends ReactActivity {
 
@@ -14,6 +18,12 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "shuttlebus";
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);
+        super.onCreate(savedInstanceState);
     }
 
     @Override
