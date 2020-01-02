b32a8c858ede6020f40c1d33bc950601193693af
MingChuan Jiang <trueshinygold@gmail.com>
2020-01-02 15:23:36 +0800

    add splash screen

--- android/app/src/main/java/com/shuttlebus/MainActivity.java
+++ android/app/src/main/java/com/shuttlebus/MainActivity.java
@@ -7,8 +7,6 @@ import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
-
-
 public class MainActivity extends ReactActivity {
 
     /**
